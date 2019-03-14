57897fc398c47475680321294057b2d7f9c2e04b
Alex7498-SV <00068817>
2019-03-14 11:15:18 -0600

    Finalizacion del trabajo Grupal

---
+++ app/src/main/java/com/alex7498_sv/trabajogrupal/MainActivity.java
@@ -0,0 +1,53 @@
+package com.alex7498_sv.trabajogrupal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    int number = 0;
+    int number2 = 0;
+    private Button b1;
+    private TextView TV1;
+    private Button b2;
+    private TextView TV2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1 = findViewById(R.id.b1);
+        TV1 = findViewById(R.id.tv1);
+
+        b2 = findViewById(R.id.b2);
+        TV2 = findViewById(R.id.tv2);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number = number + 1;
+
+                TV1.setText(Integer.toString(number));
+
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number2 = number2 + 1;
+
+                TV2.setText(Integer.toString(number2));
+                
+
+            }
+        });
+    }
+
+}
